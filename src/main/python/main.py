7f0f453216f6bf5796a6ad81b7e89c61c16f5b42
SUZUKI Kei <todokimasen@gmail.com>
2018-02-03 17:54:27 +0900

    適切なファイル名に変更.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -5,7 +5,7 @@ from table_defs import load_table_defs
 def main():
     root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
     resources_dir = os.path.join(root_dir, "src", "main", "resources")
-    table_defs_file_path = os.path.join(resources_dir, "table-defs.yml")
+    table_defs_file_path = os.path.join(resources_dir, "tables.yml")
 
     for table_def in load_table_defs(table_defs_file_path):
         print(table_def)
