513b34045d2d2323f2cd9ab333e8b6859f44c59a
SUZUKI Kei <todokimasen@gmail.com>
2018-02-03 19:09:58 +0900

    設定ファイルを置くディレクトリの名前を resources から config に変更.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -3,12 +3,15 @@ import os
 from table_defs import load_table_defs
 
 def main():
-    root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
-    resources_dir = os.path.join(root_dir, "src", "main", "resources")
-    table_defs_file_path = os.path.join(resources_dir, "tables.yml")
+    table_defs = load_table_defs(config_file_path("tables.yml"))
 
-    for table_def in load_table_defs(table_defs_file_path):
+    for table_def in table_defs:
         print(table_def)
+
+def config_file_path(name):
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    root_dir = os.path.abspath(os.path.join(base_dir, "..", "..", ".."))
+    return os.path.join(root_dir, "config", name)
 
 if __name__ == "__main__":
     main()
