6b6dba28072782e5b65ca633b42c25c0fdd8cc88
SUZUKI Kei <todokimasen@gmail.com>
2018-02-12 23:41:25 +0900

    BigQuery のテーブル定義をファイルとして出力するように変更.

--- src/main/python/application.py
+++ src/main/python/application.py
@@ -29,10 +29,7 @@ class Application(object):
         connection_factory = self._create_connection_factory()
         table_defs = self._create_table_defs(connection_factory)
         for table_def in table_defs:
-            #print(bigquery.generate_table_schema_json(table_def))
-            output_file_name = "{}.{}.json".format(table_def.database, table_def.table)
-            output_file_path = os.path.join(self._config["work_dir"], output_file_name)
-            print(output_file_path)
+            self._create_bigquery_table_schema_json_file(table_def)
 
     def _create_connection_factory(self):
         return ConnectionFactory(self._config["databases"])
@@ -40,6 +37,23 @@ class Application(object):
     def _create_table_defs(self, connection_factory):
         host_db_table_list = generate_paths(self._config["tables"])
         return table_defs.create_table_defs(host_db_table_list, connection_factory)
+
+    def _create_bigquery_table_schema_json_file(self, table_def):
+        file_path = self._get_bigquery_table_schema_json_file_path(table_def)
+        makedirs(os.path.dirname(file_path))
+        with open(file_path, "w") as file:
+            file.write(bigquery.generate_table_schema_json(table_def))
+
+    def _get_bigquery_table_schema_json_file_path(self, table_def):
+        file_name = "{}.{}.json".format(table_def.database, table_def.table)
+        return os.path.join(self._config["work_dir"], "bigquery", file_name)
+
+def makedirs(path):
+    try:
+        os.makedirs(path)
+    except OSError as error:
+        if error.errno != 17:
+            raise
 
 def main():
     Application().run()
