2e45a3ddd25e87db1bb4516d274949ca575d9981
SUZUKI Kei <todokimasen@gmail.com>
2018-02-12 23:47:08 +0900

    Invoke のタスク名を整理.

--- tasks.py
+++ tasks.py
@@ -5,8 +5,8 @@ def run(context):
     """アプリケーションを実行する."""
     context.run("python src/main/python/application.py --config-dir=./config")
 
-@task(name="test:unit")
-def test(context):
+@task(name="test:unit", aliases=["test"])
+def run_unit_test(context):
     """単体テストを実行する."""
     context.run("python -m unittest discover -s src/test/python -t .")
 
